e3124174b5b74f4734f152543ee722874e01af59
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-21 02:32:39 -0700

    Add manifest

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,25 @@
+# script to configure nginx on new ubuntu machine
+
+$replace = "	location /redirect_me/ {
+		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+	}"
+
+package {'nginx':
+ensure => installed,
+}
+
+file { '/var/www/html/index.html':
+ensure => present,
+path => '/var/www/html/index.html',
+content => 'Hello World!',
+}
+
+exec { 'redirect me':
+group => 'root',
+command => 'sed -i "47s/^$/$replace"',
+path => '/etc/nginx/sites-available/default',
+}
+
+package { 'nginx':
+ensure: restart,
+}
