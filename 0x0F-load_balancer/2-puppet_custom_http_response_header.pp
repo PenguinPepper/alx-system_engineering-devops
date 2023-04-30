1360ba49ec4ce5e56e79097cbd06c7961fce3638
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-04-30 10:40:36 -0700

    Remove extra file_line block

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -8,25 +8,6 @@ ensure => 'running',
 enable => true,
 }
 
-$replace = "    location /redirect_me/ {
-                return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-        }"
-
-file { '/var/www/html/index.html':
-ensure  => present,
-path    => '/var/www/html/index.html',
-content => 'Hello World!',
-notify  => Service['nginx'],
-}
-
-file_line { 'redirect_me':
-ensure => present,
-path   => '/etc/nginx/sites-available/default',
-after  => 'server_name _;',
-line   => $replace,
-notify => Service['nginx'],
-}
-
 $header = "	add_header X-Served-By '\$hostname';"
 
 file_line {'add_header':
