c6b5b382277fe88883f374bad53eb51f972e8434
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-08 17:23:21 -0700

    Add files

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module contains the function that gets the number of subscrbers
+"""
+
+import requests
+
+
+def top_ten((subreddit):
+    """
+    Function that prints the titles of the
+    first 10 hot posts listed for a given subreddit
+    """
+    if not subreddit:
+    print(None)
+    else:
+        return 1
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module contains the function that gets the number of subscrbers
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Function that prints the titles of the
+    first 10 hot posts listed for a given subreddit
+    """
+    if not subreddit:
+        return None
+    else:
+        return hot_list
