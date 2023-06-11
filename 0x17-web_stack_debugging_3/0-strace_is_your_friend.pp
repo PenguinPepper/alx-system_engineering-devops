facf8b41d1ac8ef05865cd76304f0ab63c853126
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-11 14:11:44 -0700

    Fix the puppet error command not found by providing the proper path

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # script that fixes Apache2 error code 500
 
 exec {'fix typo':
-command => 'sed -i "s/phpp/php/g" wp-settings.php',
-path     => '/var/www/html',
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+path    => ['/bin', '/sbin', '/usr/bin', '/usr/sbin'],
 }
