e0164190eeb0083886dbd62631ffa3b1c61cb5ab
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-06-14 14:04:12 -0700

    Add script that comments our a line in security.limt folder to increase the max number of open files

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,9 @@
+ # scrip that changes user limit
+
+$the_line = 'holberton hard nofile 5'
+
+comment_line{'comment out a line':
+ensure => commented,
+path   => '/etc/security/limits.conf',
+match  => $the_line,
+}
