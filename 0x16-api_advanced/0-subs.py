352b95a80b818153376d650e6421552243a07d33
Elizabeth Pfachi <testlolabtest@gmail.com>
2023-08-10 07:17:18 -0700

    Edit file to follow PEP8 standards

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,6 +4,7 @@
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """
     Function queries the Reddit API and returns
@@ -16,7 +17,10 @@ def number_of_subscribers(subreddit):
     """
     base_url = 'https://www.reddit.com/'
     reddit_r = 'r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188'}
+    headers = {'User-Agent':
+               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) \
+               AppleWebKit/537.36 (KHTML, like Gecko) \
+               Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188'}
 
     r = requests.get(base_url + reddit_r, headers=headers)
     if r.status_code == 200:
